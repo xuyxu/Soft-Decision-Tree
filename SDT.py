d2b4fef400a8a83a3de26a6385f2108fe5af7da7
AaronX121 <xuyx@lamda.nju.edu.cn>
2020-12-23 13:35:20 +0800

    [ENH] flake8 formatting

--- SDT.py
+++ SDT.py
@@ -3,171 +3,169 @@ import torch.nn as nn
 
 
 class SDT(nn.Module):
-    """ Fast implementation of soft decision tree using PyTorch.
+    """Fast implementation of soft decision tree in PyTorch.
 
     Parameters
     ----------
     input_dim : int
       The number of input dimensions.
     output_dim : int
-      The number of output dimensions. For example, for a multi-class 
+      The number of output dimensions. For example, for a multi-class
       classification problem with `K` classes, it is set to `K`.
     depth : int, default=5
       The depth of the soft decision tree. Since the soft decision tree is
-      a full binary tree, setting `depth` to a large value drastically 
-      increases the training and evaluating costs.
+      a full binary tree, setting `depth` to a large value will drastically
+      increases the training and evaluating cost.
     lamda : float, default=1e-3
       The coefficient of the regularization term in the training loss. Please
-      see the original paper for detailed design on the regularization term.
+      refer to the paper on the formulation of the regularization term.
     use_cuda : bool, default=False
       When set to `True`, use GPU to fit the model. Training a soft decision
-      tree using CPU can be faster considering the inherent data forwarding
+      tree using CPU could be faster considering the inherent data forwarding
       process.
-    
+
     Attributes
     ----------
     internal_node_num_ : int
       The number of internal nodes in the tree. Given the tree depth `d`, it
-      equals to $2^d - 1$.
+      equals to :math:`2^d - 1`.
     leaf_node_num_ : int
       The number of leaf nodes in the tree. Given the tree depth `d`, it equals
-      to $2^d$.
+      to :math:`2^d`.
     penalty_list : list
-      A Python list that stores the layer-wise coefficients of the 
-      regularization term. 
-    inner_nodes : nn.Sequential
+      A list storing the layer-wise coefficients of the regularization term.
+    inner_nodes : torch.nn.Sequential
       A container that simulates all internal nodes in the soft decision tree.
-      The sigmoid activation function is concatenated to simulate the 
+      The sigmoid activation function is concatenated to simulate the
       probabilistic routing mechanism.
-    leaf_nodes : nn.Linear
-      A `nn.Linear` module that simulates all leaf nodes in the soft decision
-      tree.
+    leaf_nodes : torch.nn.Linear
+      A `nn.Linear` module that simulates all leaf nodes in the tree.
     """
 
-    def __init__(self,
-                 input_dim,
-                 output_dim,
-                 depth=5, 
-                 lamda=1e-3,
-                 use_cuda=False):
+    def __init__(
+            self,
+            input_dim,
+            output_dim,
+            depth=5,
+            lamda=1e-3,
+            use_cuda=False):
         super(SDT, self).__init__()
-        
+
         self.input_dim = input_dim
         self.output_dim = output_dim
-        
+
         self.depth = depth
         self.lamda = lamda
-        self.device = torch.device('cuda' if use_cuda else 'cpu')
-        
+        self.device = torch.device("cuda" if use_cuda else "cpu")
+
         self._validate_parameters()
-        
+
         self.internal_node_num_ = 2 ** self.depth - 1
         self.leaf_node_num_ = 2 ** self.depth
-        
+
         # Different penalty coefficients for nodes in different layers
-        self.penalty_list = [self.lamda * (2 ** (-depth)) 
-                             for depth in range(0, self.depth)] 
-        
-        # Initialize internal nodes and leaf nodes, the input dimension on 
+        self.penalty_list = [
+            self.lamda * (2 ** (-depth)) for depth in range(0, self.depth)
+        ]
+
+        # Initialize internal nodes and leaf nodes, the input dimension on
         # internal nodes is added by 1, serving as the bias.
         self.inner_nodes = nn.Sequential(
-            nn.Linear(self.input_dim + 1, 
-                      self.internal_node_num_, bias=False),
-            nn.Sigmoid())
-        
-        self.leaf_nodes = nn.Linear(self.leaf_node_num_, 
-                                    self.output_dim, bias=False)
-    
+            nn.Linear(self.input_dim + 1, self.internal_node_num_, bias=False),
+            nn.Sigmoid(),
+        )
+
+        self.leaf_nodes = nn.Linear(self.leaf_node_num_,
+                                    self.output_dim,
+                                    bias=False)
+
     def forward(self, X, is_training_data=False):
-        
         _mu, _penalty = self._forward(X)
         y_pred = self.leaf_nodes(_mu)
-        
+
         # When `X` is the training data, the model also returns the penalty
         # to compute the training loss.
         if is_training_data:
-            return y_pred, _penalty 
+            return y_pred, _penalty
         else:
             return y_pred
-    
-    """ 
-      Implementation on the data forwarding process in the soft decision tree. 
-    """
+
     def _forward(self, X):
-        
+        """Implementation on the data forwarding process."""
+
         batch_size = X.size()[0]
-        
         X = self._data_augment(X)
-        
+
         path_prob = self.inner_nodes(X)
         path_prob = torch.unsqueeze(path_prob, dim=2)
-        path_prob = torch.cat((path_prob, 1-path_prob), dim=2)
-        
-        _mu = X.data.new(batch_size,1,1).fill_(1.)
-        _penalty = torch.tensor(0.).to(self.device)
-        
-        # Iterate through internal odes in each layer to compute the final path 
+        path_prob = torch.cat((path_prob, 1 - path_prob), dim=2)
+
+        _mu = X.data.new(batch_size, 1, 1).fill_(1.0)
+        _penalty = torch.tensor(0.0).to(self.device)
+
+        # Iterate through internal odes in each layer to compute the final path
         # probabilities and the regularization term.
         begin_idx = 0
         end_idx = 1
-        
+
         for layer_idx in range(0, self.depth):
             _path_prob = path_prob[:, begin_idx:end_idx, :]
-            
-            # Extract internal nodes in the current layer to compute the 
+
+            # Extract internal nodes in the current layer to compute the
             # regularization term
             _penalty = _penalty + self._cal_penalty(layer_idx, _mu, _path_prob)
             _mu = _mu.view(batch_size, -1, 1).repeat(1, 1, 2)
-            
+
             _mu = _mu * _path_prob  # update path probabilities
-            
+
             begin_idx = end_idx
-            end_idx = begin_idx + 2 ** (layer_idx+1)
-        
+            end_idx = begin_idx + 2 ** (layer_idx + 1)
+
         mu = _mu.view(batch_size, self.leaf_node_num_)
-        
-        return mu, _penalty          
-    
-    """ 
-      Compute the regularization term for internal nodes in different layers. 
-    """
+
+        return mu, _penalty
+
     def _cal_penalty(self, layer_idx, _mu, _path_prob):
-        
-        penalty = torch.tensor(0.).to(self.device)   
-        
+        """
+        Compute the regularization term for internal nodes in different layers.
+        """
+
+        penalty = torch.tensor(0.0).to(self.device)
+
         batch_size = _mu.size()[0]
-        _mu = _mu.view(batch_size, 2**layer_idx)
-        _path_prob = _path_prob.view(batch_size, 2**(layer_idx+1))
-        
-        for node in range(0, 2**(layer_idx+1)):
-            alpha = (torch.sum(_path_prob[:, node] * _mu[:, node//2], dim=0) / 
-                     torch.sum(_mu[:, node//2], dim=0))
-            
-            layer_penalty_coeff = self.penalty_list[layer_idx]
-            
-            penalty -= 0.5 * layer_penalty_coeff * (torch.log(alpha) + 
-                                                    torch.log(1-alpha))
-        
+        _mu = _mu.view(batch_size, 2 ** layer_idx)
+        _path_prob = _path_prob.view(batch_size, 2 ** (layer_idx + 1))
+
+        for node in range(0, 2 ** (layer_idx + 1)):
+            alpha = torch.sum(
+                _path_prob[:, node] * _mu[:, node // 2], dim=0
+            ) / torch.sum(_mu[:, node // 2], dim=0)
+
+            coeff = self.penalty_list[layer_idx]
+
+            penalty -= 0.5 * coeff * (torch.log(alpha) + torch.log(1 - alpha))
+
         return penalty
-    
-    """ 
-      Add a constant input `1` onto the front of each instance. 
-    """
+
     def _data_augment(self, X):
+        """Add a constant input `1` onto the front of each sample."""
         batch_size = X.size()[0]
         X = X.view(batch_size, -1)
         bias = torch.ones(batch_size, 1).to(self.device)
         X = torch.cat((bias, X), 1)
-        
+
         return X
-    
+
     def _validate_parameters(self):
-        
+
         if not self.depth > 0:
-          msg = 'The tree depth should be strictly positive, but got {} instead.'
-          raise ValueError(msg.format(self.depth))
+            msg = ("The tree depth should be strictly positive, but got {}"
+                   "instead.")
+            raise ValueError(msg.format(self.depth))
 
         if not self.lamda >= 0:
-          msg = ('The coefficient of the regularization term should not be' 
-                 ' negative, but got {} instead.')
-          raise ValueError(msg.format(self.lamda))
+            msg = (
+                "The coefficient of the regularization term should not be"
+                " negative, but got {} instead."
+            )
+            raise ValueError(msg.format(self.lamda))
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-""" Training and evaluating a soft decision tree on the MNIST dataset. """
+"""Training and evaluating a soft decision tree on the MNIST dataset."""
 
 import torch
 import torch.nn as nn
@@ -9,112 +9,125 @@ from SDT import SDT
 
 
 def onehot_coding(target, device, output_dim):
-    """ 
-      Convert the class labels into one-hot encoded vectors.
-    """
+    """Convert the class labels into one-hot encoded vectors."""
     target_onehot = torch.FloatTensor(target.size()[0], output_dim).to(device)
     target_onehot.data.zero_()
-    target_onehot.scatter_(1, target.view(-1, 1), 1.)
+    target_onehot.scatter_(1, target.view(-1, 1), 1.0)
     return target_onehot
 
 
-if __name__ == '__main__':
-    
+if __name__ == "__main__":
+
     # Parameters
-    input_dim = 28 * 28
-    output_dim = 10
-    depth = 5
-    lamda = 1e-3
-    use_cuda = False
-    
-    lr = 1e-3
-    weight_decaly = 5e-4
-    
-    batch_size = 128
-    epochs = 50
-    log_interval = 100
-    
+    input_dim = 28 * 28    # the number of input dimensions
+    output_dim = 10        # the number of outputs (i.e., # classes on MNIST)
+    depth = 5              # tree depth
+    lamda = 1e-3           # coefficient of the regularization term
+    lr = 1e-3              # learning rate
+    weight_decaly = 5e-4   # weight decay
+    batch_size = 128       # batch size
+    epochs = 50            # the number of training epochs
+    log_interval = 100     # the number of batches to wait before printing logs
+    use_cuda = False       # whether to use GPU
+
     # Model and Optimizer
     tree = SDT(input_dim, output_dim, depth, lamda, use_cuda)
-    
-    optimizer = torch.optim.Adam(tree.parameters(), 
-                                 lr=lr, 
+
+    optimizer = torch.optim.Adam(tree.parameters(),
+                                 lr=lr,
                                  weight_decay=weight_decaly)
-    
+
     # Load data
-    data_dir = '../Dataset/mnist'
-    
-    transformer = transforms.Compose([transforms.ToTensor(),
-                                      transforms.Normalize((0.1307,),
-                                                           (0.3081,))])
-    
+    data_dir = "../Dataset/mnist"
+
+    transformer = transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
+    )
+
     train_loader = torch.utils.data.DataLoader(
-        datasets.MNIST(data_dir, train=True, download=True, 
-                       transform=transformer), batch_size=batch_size, 
-                       shuffle=True)
+        datasets.MNIST(data_dir,
+                       train=True,
+                       download=True,
+                       transform=transformer),
+        batch_size=batch_size,
+        shuffle=True,
+    )
 
     test_loader = torch.utils.data.DataLoader(
-        datasets.MNIST(data_dir, train=False, 
-                       transform=transformer), batch_size=batch_size,
-                       shuffle=True)
-    
+        datasets.MNIST(data_dir,
+                       train=False,
+                       transform=transformer),
+        batch_size=batch_size,
+        shuffle=True,
+    )
+
     # Utils
-    best_testing_acc = 0.
+    best_testing_acc = 0.0
     testing_acc_list = []
     training_loss_list = []
     criterion = nn.CrossEntropyLoss()
-    device = torch.device('cuda' if use_cuda else 'cpu')
-    
+    device = torch.device("cuda" if use_cuda else "cpu")
+
     for epoch in range(epochs):
-        
-        # Training stage
+
+        # Training
         tree.train()
         for batch_idx, (data, target) in enumerate(train_loader):
-            
+
             batch_size = data.size()[0]
             data, target = data.to(device), target.to(device)
             target_onehot = onehot_coding(target, device, output_dim)
-            
+
             output, penalty = tree.forward(data, is_training_data=True)
 
             loss = criterion(output, target.view(-1))
             loss += penalty
-            
+
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-            
+
             # Print training status
             if batch_idx % log_interval == 0:
                 pred = output.data.max(1)[1]
                 correct = pred.eq(target.view(-1).data).sum()
-                
-                msg = ('Epoch: {:02d} | Batch: {:03d} | Loss: {:.5f} |'
-                       ' Correct: {:03d}/{:03d}')
+
+                msg = (
+                    "Epoch: {:02d} | Batch: {:03d} | Loss: {:.5f} |"
+                    " Correct: {:03d}/{:03d}"
+                )
                 print(msg.format(epoch, batch_idx, loss, correct, batch_size))
                 training_loss_list.append(loss.cpu().data.numpy())
-        
-        # Compute the testing accuracy after each training epoch
+
+        # Evaluating
         tree.eval()
         correct = 0.
-        
+
         for batch_idx, (data, target) in enumerate(test_loader):
-            
+
             batch_size = data.size()[0]
             data, target = data.to(device), target.to(device)
-            
+
             output = F.softmax(tree.forward(data), dim=1)
-            
+
             pred = output.data.max(1)[1]
             correct += pred.eq(target.view(-1).data).sum()
-            
-        accuracy = 100. * float(correct) / len(test_loader.dataset)
-        
+
+        accuracy = 100.0 * float(correct) / len(test_loader.dataset)
+
         if accuracy > best_testing_acc:
             best_testing_acc = accuracy
-        
-        msg = ('\nEpoch: {:02d} | Testing Accuracy: {}/{} ({:.3f}%) |'
-               ' Historical Best: {:.3f}%\n')
-        print(msg.format(epoch, correct, len(test_loader.dataset),
-                         accuracy, best_testing_acc))
+
+        msg = (
+            "\nEpoch: {:02d} | Testing Accuracy: {}/{} ({:.3f}%) |"
+            " Historical Best: {:.3f}%\n"
+        )
+        print(
+            msg.format(
+                epoch, correct,
+                len(test_loader.dataset),
+                accuracy,
+                best_testing_acc
+            )
+        )
         testing_acc_list.append(accuracy)
